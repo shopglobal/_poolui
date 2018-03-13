60dfdb3584e977888ae228eebebc3942572945b4
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-13 17:58:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-var api = "http://127.0.0.1:8117";
-var coinUnits = 1000000000000;
-var poolHost = "Coinmine.network";
+var api = "//127.0.0.1:8117";
+var coinUnits = 1000000000;
+var poolHost = "pool.kryptonium.io";
 
-var irc = "irc.freenode.net/#mynt-pools";
+var irc = "irc.freenode.net/#kryptonium-pools";
 
-var email = "support@Coinmine.network";
+var email = "support@kryptonium.io";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
-var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+var easyminerDownload = "#";
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var blockchainExplorer = "https://explorer.kryptonium.io/block/{id}";
 
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var transactionExplorer = "https://explorer.kryptonium.io/txid/{id}";
 
-var themeCss = "themes/theme.css";
+var themeCss = "themes/default.css";
