523f5433b1ce5e82179ef8517694a81ccdafdbff
Interchained <president@worldvaporexpo.com>
2018-04-29 19:30:43 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,17 +1,17 @@
 var api = "//165.227.189.226:8117";
-var coinUnits = 1000000000000;
-var poolHost = "Coinmine.network";
+var coinUnits = 100;
+var poolHost = "pool.electronero.org";
 
-var irc = "irc.freenode.net/#mynt-pools";
+var irc = "irc.freenode.net/#etnx-pools";
 
-var email = "support@Coinmine.network";
+var email = "electronero.official@gmail.com";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "#";
 
-var blockchainExplorer = "https://explorer.myntpulse.com/block/{id}";
+var blockchainExplorer = "https://blockexplorer.electroneropulse.org/block/{id}";
 
-var transactionExplorer = "https://explorer.myntpulse.com/txid/{id}";
+var transactionExplorer = "https://blockexplorer.electroneropulse.org/txid/{id}";
 
-var themeCss = "themes/default-theme.css";
+var themeCss = "themes/default.css";
