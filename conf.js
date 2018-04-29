389aa6389cb35eb03fb829c270891756debd4388
Mark Allen Evans <president@worldvaporexpo.com>
2018-04-29 23:20:54 +0000

    cleanup

---
+++ conf.js
@@ -0,0 +1,17 @@
+var api = "//165.227.189.226:8117";
+var coinUnits = 1000000000000;
+var poolHost = "Coinmine.network";
+
+var irc = "irc.freenode.net/#mynt-pools";
+
+var email = "support@Coinmine.network";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "#";
+
+var blockchainExplorer = "https://explorer.myntpulse.com/block/{id}";
+
+var transactionExplorer = "https://explorer.myntpulse.com/txid/{id}";
+
+var themeCss = "themes/default-theme.css";
